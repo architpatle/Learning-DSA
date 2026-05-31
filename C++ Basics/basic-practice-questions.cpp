f1fe894880613b0299cc9ff158b119a8be7c3267
Archit Patle <architpatle06@gmail.com>
2026-06-01 01:44:08 +0530

    Practised a very basic practice question

---
+++ C++ Basics/basic-practice-questions.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1, num2;
+
+    cout << "Enter number 1:";
+    cin >> num1;
+
+    cout << "Enter number 2:";
+    cin >> num2;
+
+    cout << "Sum of " << num1 << " & " << num2 << " is: " << num1 + num2;
+    return 0;
+}
