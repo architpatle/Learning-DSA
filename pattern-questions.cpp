3d004a56475d3e10c8f328146dd0ed90b51608b5
Archit Patle <architpatle06@gmail.com>
2026-06-04 19:55:56 +0530

    Practiced Hollow Diamond pattern problems

--- pattern-questions.cpp
+++ pattern-questions.cpp
@@ -253,13 +253,41 @@ int main()
 
     for (int i = 0; i < n; i++)
     {
-        for(int j = 0; j<n*2-1; j++){
-         if(j==n-i-1 || j==0 || j==n+i){
-            cout<<"*";
-         }else{
-            cout<<" ";
-         }
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            cout << "  ";
         }
+        cout << "*" << " ";
+
+        if (i != 0)
+        {
+            for (int j = 0; j < 2 * i - 1; j++)
+            {
+                cout << "  ";
+            }
+            cout << "*" << " ";
+        }
+
+        cout << endl;
+    }
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < i + 1; j++)
+        {
+            cout << "  ";
+        }
+        cout << "*" << " ";
+
+        if (i != n - 2)
+        {
+            for (int j = 0; j < 2*(n-i) - 5; j++)
+            {
+                cout << "  ";
+            }
+            cout << "*" << " ";
+        }
+
         cout << endl;
     }
 
