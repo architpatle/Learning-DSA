5fea969d9d935260a955d30e09197f3a6197a387
Archit Patle <architpatle06@gmail.com>
2026-06-06 21:28:53 +0530

    Practiced Binary Number System questions

--- binaryNumberSystem.cpp
+++ binaryNumberSystem.cpp
@@ -33,10 +33,45 @@ int binaryToDecimal(int binNum)
     return ans;
 }
 
+int dtb(int decNum)
+{
+    int ans = 0, pow = 1;
+
+    while (decNum > 0)
+    {
+        int rem = decNum % 2;
+        decNum /= 2;
+
+        ans += rem * pow;
+
+        pow *= 10;
+    }
+    return ans;
+}
+
+int btd(int biNum)
+{
+    int ans = 0, pow = 1;
+
+    while (biNum > 0)
+    {
+        int rem = biNum % 10;
+        biNum /= 10;
+
+        ans += rem * pow;
+
+        pow *= 2;
+    }
+    return ans;
+}
+
 int main()
 {
     // cout << decimalToBinary(50);
-    cout<< binaryToDecimal(110010);
+    // cout<< binaryToDecimal(110010);
+
+    // cout << dtb(40);
+    cout<< btd(1010);
 
     return 0;
 }
