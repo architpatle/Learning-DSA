ecc4088df18b8380bdeda110c5f4eb006b241c17
Archit Patle <architpatle06@gmail.com>
2026-06-10 15:35:53 +0530

    Practiced Vectors Questions

--- Data Structures/vector.cpp
+++ Data Structures/vector.cpp
@@ -2,6 +2,48 @@
 #include <vector>
 using namespace std;
 
+// Linear Search using vector
+void linearSearch(vector<int> vec, int target)
+{
+    bool targetFound = false;
+    int index;
+
+    for (int val : vec)
+    {
+        if (val == target)
+        {
+            targetFound = true;
+            // index = vec.at();
+            break;
+        }
+    }
+
+    if (targetFound)
+    {
+        cout << "Target found";
+    }
+    else
+    {
+        cout << "Target Not Present";
+    }
+}
+
+// Changing array
+void reverseAnArray(vector<int>& vec)
+{
+    int start = 0, end = vec.size() - 1;
+
+    while(start<end){
+        swap(vec[start], vec[end]);
+        start++;
+        end--;
+    }
+
+    for( int val: vec){
+        cout<<val<<" ";
+    }
+}
+
 int main()
 {
     // vector<int> vec(4, 30);
@@ -23,7 +65,27 @@ int main()
     // int val = 6 ^ 6;
 
     // cout<<val;
-    cout<<ans;
+    // cout << ans;
 
+    // linearSearch(vec, 3);
+
+    cout << "original Array: ";
+
+    for (int val : vec)
+    {
+        cout << val << " ";
+    }
+    cout << endl;
+    cout << "Change Array: ";
+
+    reverseAnArray(vec);
+
+    cout << endl;
+    cout << "Main array after change: ";
+
+    for (int val : vec)
+    {
+        cout << val << " ";
+    }
     return 0;
 }
