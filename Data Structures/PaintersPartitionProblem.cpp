0b7b881f33427a73abbae086d391560089a3d44d
Archit Patle <architpatle06@gmail.com>
2026-07-04 11:06:29 +0530

    Practiced Painter's Partition Problem

--- Data Structures/PaintersPartitionProblem.cpp
+++ Data Structures/PaintersPartitionProblem.cpp
@@ -4,9 +4,10 @@ using namespace std;
 
 // ----- Painter's Partition Problem -----
 /*  1. In this An array is given, each value tells the number of minutes it will take to paint the particular boat
-    2. n = number of boats, m = Number of painters
-    3. Objective is to find minimum duration to paint all the boats
-    4. TC : O(log sum * n) 
+    2. Given n = number of boats, m = Number of painters
+    3. Boats can be painted in contiguous Manner only
+    4. Objective is to find minimum duration to paint all the boats
+    5. TC : O(log sum * n)
 */
 
 bool isPossible(vector<int> arr, int n, int m, int maxAllowedTime)
@@ -26,7 +27,7 @@ bool isPossible(vector<int> arr, int n, int m, int maxAllowedTime)
         }
     }
 
-    return painter<=m;
+    return painter <= m;
 }
 
 int minTimeToPaint(vector<int> arr, int n, int m)
@@ -44,7 +45,7 @@ int minTimeToPaint(vector<int> arr, int n, int m)
     // Applying Binary Search on Range of values Array
 
     int start = maxVal, end = sum, ans = -1;
-    while (start <= end) // O(log sum * n) 
+    while (start <= end) // O(log sum * n)
     {
         int mid = start + ((end - start) / 2);
 
@@ -64,11 +65,67 @@ int minTimeToPaint(vector<int> arr, int n, int m)
     return ans;
 }
 
+// ----- Practice ----
+
+bool isPossiblePractice(vector<int> arr, int n, int m, int maxAllowedTime)
+{
+
+    int painters = 1, time = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (time + arr[i] <= maxAllowedTime)
+        {
+            time += arr[i];
+        }
+        else
+        {
+            painters++;
+            time = arr[i];
+        }
+    }
+
+    return painters <= m;
+}
+
+int minTimeToPaintPractice(vector<int> arr, int n, int m)
+{
+
+    int maxVal = INT32_MIN, sum = 0;
+
+    for (int val : arr)
+    {
+        maxVal = max(maxVal, val);
+        sum += val;
+    }
+
+    int start = maxVal, end = sum, ans = -1;
+
+    while (start <= end)
+    {
+        int mid = start + ((end - start) / 2);
+
+        if (isPossiblePractice(arr, n, m, mid))
+        {
+            ans = mid;
+            end = mid - 1;
+        }
+        else
+        {
+            start = mid + 1;
+        }
+    }
+
+    return ans;
+}
+
 int main()
 {
-    vector<int> boats = {40, 30, 20, 10};
+    vector<int> boats = {40, 80, 20, 10};
     int n = 4, m = 2;
 
-    cout << minTimeToPaint(boats, n, m);
+    cout << minTimeToPaint(boats, n, m) << endl;
+
+    cout << minTimeToPaintPractice(boats, n, m);
     return 0;
 }
