598f648a47ac75c7d543eee7ce00403ff726a464
Archit Patle <architpatle06@gmail.com>
2026-07-06 11:44:36 +0530

    Practiced Searching Single Element In Sorted Array Problem

--- Data Structures/Practice.cpp
+++ Data Structures/Practice.cpp
@@ -22,6 +22,7 @@ using namespace std;
  16. Binary Search Algorithm
  17. Search in Rotated Sorted Array
  18. Searching Peak Index in Mountain Array
+ 19. Searching Single Element in Sorted Array ( where other Elements are in pair of two [2,2,3,3,5,6,6,8,8])
 
 
 */
@@ -440,11 +441,63 @@ int SearchingPeakIndexInMountainArray(vector<int> arr)
             return mid;
         }
 
-        if (arr[mid - 1] < arr[mid] )
+        if (arr[mid - 1] < arr[mid])
         {
             start = mid + 1;
-        } else {
-            end = mid -1;
+        }
+        else
+        {
+            end = mid - 1;
+        }
+    }
+
+    return -1;
+}
+
+// 19. Searching Single Element in Sorted Array ( where other Elements are in pair of two [2,2,3,3,5,6,6,8,8])
+int SingleElementInSortedArray(vector<int> arr)
+{
+    int start = 0, end = arr.size() - 1;
+
+    if (arr.size() == 1)
+        return arr[0];
+
+    while (start <= end)
+    {
+        int mid = start + ((end - start) / 2);
+
+        if (mid == 0 && arr[mid] != arr[mid + 1])
+        {
+            return arr[mid];
+        }
+        if (mid == arr.size() - 1 && arr[mid] != arr[arr.size() - 2])
+        {
+            return arr[mid];
+        }
+
+        if(arr[mid] != arr[mid-1] && arr[mid] != arr[mid+1]) return arr[mid];
+
+        if (mid % 2 == 0)
+        {
+            if (arr[mid] == arr[mid - 1])
+            {
+                end = mid - 1;
+            }
+            else
+            {
+                start = mid + 1;
+            }
+        }
+        else
+        {
+            if (arr[mid] == arr[mid - 1])
+            {
+                start = mid + 1;
+            }
+            else
+            {
+                end = mid - 1;
+            }
         }
     }
 
@@ -453,10 +506,10 @@ int SearchingPeakIndexInMountainArray(vector<int> arr)
 
 int main()
 {
-    vector<int> nums = {3, 6, 7, 9, 10, 1, 2};
+    vector<int> nums = {2, 5,5, 6, 6, 7, 7, 8, 8};
     vector<int> heights = {5, 2, 8, 1, 8};
 
-    cout << SearchingPeakIndexInMountainArray(nums);
+    cout << SingleElementInSortedArray(nums);
 
     return 0;
 }
