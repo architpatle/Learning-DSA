bfcde4b228336edec52eaf79547613392a07d03a
Archit Patle <architpatle06@gmail.com>
2026-07-08 13:20:48 +0530

    Practiced Dutch National Algorithm

--- Data Structures/DNF-SortingAlgorithm.cpp
+++ Data Structures/DNF-SortingAlgorithm.cpp
@@ -66,11 +66,35 @@ void DNSAlgorithm(vector<int> &arr)
     }
 }
 
+void DNSAlgorithmPractice(vector<int> &arr)
+{
+    int low = 0, mid = 0, high = arr.size() - 1;
+
+    while (mid <= high)
+    {
+        if (arr[mid] == 0)
+        {
+            swap(arr[mid], arr[low]);
+            mid++;
+            low++;
+        }
+        else if (arr[mid] == 1)
+        {
+            mid++;
+        }
+        else
+        {
+            swap(arr[mid], arr[high]);
+            high--;
+        }
+    }
+}
+
 int main()
 {
     vector<int> arr = {1, 2, 1, 2, 2, 0, 0, 1, 0, 1, 2};
 
-    DNSAlgorithm(arr);
+    DNSAlgorithmPractice(arr);
 
     for (int val : arr)
     {
