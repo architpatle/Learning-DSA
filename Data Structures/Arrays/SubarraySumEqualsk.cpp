83a5ac0cd0e30096f35f91451b0a9e903ec474ea
Archit Patle <architpatle06@gmail.com>
2026-07-24 13:08:36 +0530

    Practiced Subarray Sum Equals k Problem

--- Data Structures/Arrays/SubarraySumEqualsk.cpp
+++ Data Structures/Arrays/SubarraySumEqualsk.cpp
@@ -58,13 +58,50 @@ int SubArraySumOA(vector<int> arr, int key)
     return count;
 }
 
+
+/* ----- PRACTICE ----- */
+int SubArraySumPractice(vector<int> arr, int key){
+    int n= arr.size();
+    int count = 0;
+
+    vector<int> preSum(n,0);
+    preSum[0] = arr[0];
+
+    for(int i =1; i<n; i++){
+        preSum[i] = preSum[i-1] + arr[i];
+    }
+
+    unordered_map<int,int> m;
+
+    for(int j =0; j<n; j++){
+        
+        if(preSum[j] == key) count++;
+
+        int val = preSum[j] -key;
+
+        if(m.find(val) != m.end()){
+            count += m[val];
+        }
+
+        if(m.find(val) == m.end()){
+            m[preSum[j]] = 0;
+        }
+            m[preSum[j]]++;
+
+    }
+
+    return count;
+}
+
 int main()
 {
 
-    vector<int> arr = {9, 4, 20, 3, 10, 5};
-    int k = 33;
+    vector<int> arr = {9, 4, 20, 3, 10, 5,8};
+    int k = 13;
 
-    cout << SubArraySumOA(arr, k);
+    cout << SubArraySumOA(arr, k)<<endl;
+    cout << SubArraySumPractice(arr, k)<<endl;
+
 
     return 0;
 }
