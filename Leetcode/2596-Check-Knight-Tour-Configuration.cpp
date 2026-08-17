24db663d2533273ee74e2bf71e0cd9dba86c8e4d
Archit Patle <architpatle06@gmail.com>
2026-08-17 14:54:36 +0530

    Practiced LC-2596

---
+++ Leetcode/2596-Check-Knight-Tour-Configuration.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool helper(vector<vector<int>> &grid, int r, int c, int n, int expVal)
+{
+    if (r < 0 || c < 0 || r >= n || c >= n || grid[r][c] != expVal)
+    {
+        return false;
+    }
+    if (expVal == n * n - 1)
+        return true;
+
+    int ans1 = helper(grid, r - 1, c - 2, n, expVal + 1);
+    int ans2 = helper(grid, r - 2, c - 1, n, expVal + 1);
+    int ans3 = helper(grid, r - 2, c + 1, n, expVal + 1);
+    int ans4 = helper(grid, r - 1, c + 2, n, expVal + 1);
+    int ans5 = helper(grid, r + 1, c + 2, n, expVal + 1);
+    int ans6 = helper(grid, r + 2, c + 1, n, expVal + 1);
+    int ans7 = helper(grid, r + 2, c - 1, n, expVal + 1);
+    int ans8 = helper(grid, r + 1, c - 2, n, expVal + 1);
+
+    return ans1 || ans2 || ans3 || ans4 || ans5 || ans6 || ans7 || ans8;
+}
+
+bool isValid(vector<vector<int>> &grid)
+{
+    return helper(grid, 0, 0, grid.size(), 0);
+}
+
+int main()
+{
+
+    vector<vector<int>> grid = {{0, 11, 16, 5, 20},
+                                {17, 4, 19, 10, 15},
+                                {12, 1, 8, 21, 6},
+                                {3, 18, 23, 14, 9},
+                                {24, 13, 2, 7, 22}};
+
+    cout << isValid(grid);
+    return 0;
+}
